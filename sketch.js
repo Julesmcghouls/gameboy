dc835ff0da47e702434b77ec01052a13b2f09440
Julian <jjvll90@gmail.com>
2024-09-02 14:15:49 -0500

    initial commit - set up basic p5 functionality

---
+++ sketch.js
@@ -0,0 +1,13 @@
+//Basic canvas 
+
+let winWidth = 400;
+let winHeight = 300;
+
+function setup() {
+    let canvas = createCanvas(winWidth, winHeight);
+}
+
+function draw() {
+    background(51);
+}   
+
